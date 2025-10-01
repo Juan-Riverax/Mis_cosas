960029b44e4c6ea85e20585b7bf6f8eda48215e9
Juan-Riverax <juanjisx2@gmail.com>
2025-09-30 22:26:22 -0500

    Add files via upload

---
+++ Laboratorio/C++/Condicionales/if_else/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int edad = 20;
+	if(edad>18){
+		printf("Ud es, legalmente, un adulto.\n");
+	} else {
+		printf("Ud todavia no es un adulto.\n");
+	}
+	return 0;
+}
