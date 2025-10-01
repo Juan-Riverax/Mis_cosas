cc449740caae6ec34337a0aa1b18a40a2ce283e8
Juan-Riverax <juanjisx2@gmail.com>
2025-09-30 22:21:40 -0500

    Add files via upload

---
+++ Laboratorio/C++/if/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int numero = 10;
+	if (numero<=15){
+		printf("El numero es menor que 15.\n");
+		
+	}
+	return 0;
+}
