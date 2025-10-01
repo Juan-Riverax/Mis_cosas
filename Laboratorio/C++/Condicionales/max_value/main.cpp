dcf1a016be7532cd9158b62e9e086bcc21436a62
Juan-Riverax <juanjisx2@gmail.com>
2025-09-30 22:28:38 -0500

    Add files via upload

---
+++ Laboratorio/C++/Condicionales/max_value/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int x = 30;
+	int y = 50;
+	int Valor_maximo;
+	
+	Valor_maximo = (x > y) ? x : y; 
+	printf("El valor maximo es:%d", Valor_maximo);
+	
+	return 0;
+}
